0f657f850870d37fbdcedf6d729933d32b0c801f
deokdory <deokdory@naver.com>
2024-05-30 23:33:06 +0900

    rand 크레이트

--- guess/src/main.rs
+++ guess/src/main.rs
@@ -1,9 +1,12 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
 
-    println("Please input your guess.");
+    let secret_number = rand::thread_rng().gen_range(0..=100);
+
+    println!("Please input your guess");
 
     let mut guess = String::new();
 
@@ -11,5 +14,5 @@ fn main() {
         .read_line(&mut guess)
         .expect("Failed to read line");
 
-    println("You guessed: {guess}");
+    println!("You guessed: {guess}");
 }
